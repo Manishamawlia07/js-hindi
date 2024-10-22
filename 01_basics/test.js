e84bc2f81218c4b67fbd422fddc6bcfa157a43f6
Manishamawlia07 <manishamawlia0710@gmail.com>
2024-10-22 10:47:04 +0000

    data types

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
 console.log("manisha");
+const accountId ="1234"
+let accountEmail =" Manisha@gmail.com"
+
+let accountState;
+console.table([ accountId,accountEmail, accountState]);
+/*
+let variable can be re- define but not redeclare
+let a=23;
+a=28; // this is right and 
+let a=23;
+let a=24;// this is not wrong it will show an error so we can not re-declare let variable.
+we can not redeclare and redefine both in case of const variable 
+
+variable are the basic entity where we store data in the mamory. we can say it named memory location 
+variable mai memory store hogi, variable name= memory name and the value we are giving to the variable anme is the value which is stored in that memory.
+let accountState; // here we declared the variable but did not assignd a value to it.
+
+*/
