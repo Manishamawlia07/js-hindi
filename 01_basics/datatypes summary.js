f91eb12608757c21e3526e1c8b3e26fe31941adc
Manishamawlia07 <manishamawlia0710@gmail.com>
2024-10-29 01:07:31 +0000

    data types summary

---
+++ 01_basics/datatypes summary.js
@@ -0,0 +1,37 @@
+// Two types of data types 
+
+// primitive 
+
+
+// 7 types =>    string , boolean, number , bigint, symbol, null, undefined
+
+const score = 100;
+
+const name = manisha;
+const isLoggedIn = true;
+const number= 12345678n;
+const mintemp = null;
+let average;
+
+let symbol= symbol('123')
+ let anotherSymbol= Symbol('123');
+
+// non- primitive 
+
+// array objects function
+
+//Array=>
+
+    let heroes= [ "shaktiman"," nagraj", "doga"]
+
+    let myObj = {
+        name: "hitesh",
+        age: 23,
+        // we can pass any array and functio to an object
+
+    }
+     let myFun = function(){
+        console.log("hello world");
+
+    }
+
