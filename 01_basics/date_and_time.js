d36ddee5c3a0ab15bd40669b4805fa4137b33c61
Manishamawlia07 <manishamawlia0710@gmail.com>
2024-11-15 07:57:04 +0000

    date and time

--- 01_basics/date_and_time.js
+++ 01_basics/date_and_time.js
@@ -1,12 +1,15 @@
 let myDate = new Date()
+console.log(myDate.getMonth);
 
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocalString());
-console.log(typeOf myDate());
+console.log(typeOf, myDate);
 
 let myCreatedDate = new Date(2023, 0, 23)
 console.log(myCreatedDate.toDateString());
 
 console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
 
